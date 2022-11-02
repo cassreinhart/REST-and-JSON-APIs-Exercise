7f29b135028facf09dccaef2fdfcd992ebbd2592
Cassandra Reinhart <cassandrareinhart13@gmail.com>
2022-11-02 11:56:27 -0500

    add front end UI/static files

---
+++ static/cupcakes.js
@@ -0,0 +1,19 @@
+const BASE_URL = 'localhost:5000/api'
+const $cupcakesList = $('.cupcakes ul');
+const $addCupcakeForm = $('#new-cupcake');
+
+function getCupcakeData() {
+    //make request to API to get JSON of all cupcakes
+}
+
+function cupcakeMarkup() {
+    //insert html string to dynamically put in UL
+}
+
+function putCupcakesOnPage() {
+    // manipulate DOM to add cupcakes to UI
+}
+
+function handleNewCupcakeForm() {
+    // make post request to API to submit new JSON for new cupcake
+}
